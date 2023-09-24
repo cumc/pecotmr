58a281d92cb2b2e3ae0c487ac538df883142b6a0
Gao Wang <wangow@gmail.com>
2023-09-24 15:47:34 -0400

    Finalize draft interface to Rcpp

--- src/qtl_enrichment.cpp
+++ src/qtl_enrichment.cpp
@@ -7,9 +7,9 @@
 #include <vector>
 
 std::vector<double> run_EM(
-    const std::vector<double> &gwas_pip, 
-    const std::vector<int> &qtl_sample, 
-    double pi_qtl, 
+    const std::vector<double> &gwas_pip,
+    const std::vector<int> &qtl_sample,
+    double pi_qtl,
     double pi_gwas)
 {
     double a0 = log(pi_gwas / (1 - pi_gwas));
@@ -21,31 +21,27 @@ std::vector<double> run_EM(
     double r_null = pi_gwas / (1 - pi_gwas);
     double total_snp = gwas_pip.size();
 
-    while (true)
-    {
+    while (true) {
         double pseudo_count = 1.0;
         double e0g0 = pseudo_count * (1 - pi_gwas) * (1 - pi_qtl);
         double e0g1 = pseudo_count * (1 - pi_qtl) * pi_gwas;
         double e1g0 = pseudo_count * (1 - pi_gwas) * pi_qtl;
         double e1g1 = pseudo_count * pi_gwas * pi_qtl;
 
-        for (size_t i = 0; i < gwas_pip.size(); i++)
-        {
+        for (size_t i = 0; i < gwas_pip.size(); i++) {
             double val = gwas_pip[i];
             if (val == 1)
                 val = 1 - 1e-8;
             val = val / (1 - val);
 
-            if (qtl_sample[i] == 0)
-            {
+            if (qtl_sample[i] == 0) {
                 val = r0 * (val / r_null);
                 val = val / (1 + val);
                 e0g1 += val;
                 e0g0 += 1 - val;
             }
 
-            if (qtl_sample[i] == 1)
-            {
+            if (qtl_sample[i] == 1) {
                 val = r1 * (val / r_null);
                 val = val / (1 + val);
                 e1g1 += val;
@@ -57,8 +53,7 @@ std::vector<double> run_EM(
 
         double a1_new = log(e1g1 * e0g0 / (e1g0 * e0g1));
 
-        if (fabs(a1_new - a1) < 0.01)
-        {
+        if (fabs(a1_new - a1) < 0.01) {
             a1 = a1_new;
             var1 = (1.0 / e0g0 + 1.0 / e1g0 + 1.0 / e1g1 + 1.0 / e0g1);
             var0 = (1.0 / e0g1 + 1.0 / e0g0);
@@ -82,19 +77,15 @@ std::vector<double> run_EM(
 }
 
 std::map<std::string, double> qtl_enrichment_workhorse(
-    const std::vector<SuSiEFit> &qtl_susie_fits, 
+    const std::vector<SuSiEFit> &qtl_susie_fits,
     const std::vector<double> &gwas_pip,
-    const std::vector<std::string> &gwas_pip_names, 
-    double pi_gwas, 
+    const std::vector<std::string> &gwas_pip_names,
+    double pi_gwas,
     double pi_qtl,
-    int ImpN, 
-    double prior_variance) 
+    int ImpN,
+    double prior_variance,
+    int num_threads = 4)
 {
-    const gsl_rng_type *T;
-    gsl_rng *r;
-    gsl_rng_env_setup();
-    T = gsl_rng_default;
-    r = gsl_rng_alloc(T);
 
     std::vector<double> a0_vec(ImpN, 0.0);
     std::vector<double> v0_vec(ImpN, 0.0);
@@ -107,16 +98,26 @@ std::map<std::string, double> qtl_enrichment_workhorse(
         gwas_variant_index[gwas_pip_names[i]] = i;
     }
 
-    #pragma omp parallel for num_threads(4)
+    #pragma omp parallel for num_threads(num_threads)
     for (int k = 0; k < ImpN; k++) {
-        std::vector<int> qtl_sample(gwas_pip.size(), 0.0);
+	// Initialize the GSL RNG for this thread
+	const gsl_rng_type *T;
+	gsl_rng *r;
+    	gsl_rng_env_setup();
+    	T = gsl_rng_default;
+    	r = gsl_rng_alloc(T);
+	// Set the seed for this thread's RNG
+        gsl_rng_set(r, static_cast<unsigned long int>(k));
+
+        std::vector<int> qtl_sample(gwas_pip.size(), 0);
 
         for (size_t i = 0; i < qtl_susie_fits.size(); i++) {
             std::vector<std::string> variants = qtl_susie_fits[i].impute_qtn(r);
-            for (const auto& variant : variants) {
+            for (const auto &variant : variants) {
                 qtl_sample[gwas_variant_index[variant]] = 1;
             }
         }
+    	gsl_rng_free(r);
 
         std::vector<double> rst = run_EM(gwas_pip, qtl_sample, pi_qtl, pi_gwas);
 
@@ -128,35 +129,30 @@ std::map<std::string, double> qtl_enrichment_workhorse(
             v1_vec[k] = rst[3];
         }
     }
-    gsl_rng_free(r);
 
     double a0_est = 0;
     double a1_est = 0;
     double var0 = 0;
     double var1 = 0;
-    for (int k = 0; k < ImpN; k++)
-    {
+    for (int k = 0; k < ImpN; k++) {
         a0_est += a0_vec[k];
         a1_est += a1_vec[k];
         var0 += v0_vec[k];
         var1 += v1_vec[k];
     }
-
-    a0_est = a0_est / ImpN;
-    a1_est = a1_est / ImpN;
+    a0_est /= ImpN;
+    a1_est /= ImpN;
 
     double bv0 = 0;
     double bv1 = 0;
-    for (int k = 0; k < ImpN; k++)
-    {
+    for (int k = 0; k < ImpN; k++) {
         bv0 += pow(a0_vec[k] - a0_est, 2.0);
         bv1 += pow(a1_vec[k] - a1_est, 2.0);
     }
-
-    bv0 = bv0 / (ImpN - 1);
-    bv1 = bv1 / (ImpN - 1);
-    var0 = var0 / ImpN;
-    var1 = var1 / ImpN;
+    bv0 /= (ImpN - 1);
+    bv1 /= (ImpN - 1);
+    var0 /= ImpN;
+    var1 /= ImpN;
 
     double sd0 = sqrt(var0 + bv0 * (ImpN + 1) / ImpN);
     double sd1 = sqrt(var1 + bv1 * (ImpN + 1) / ImpN);
@@ -164,9 +160,8 @@ std::map<std::string, double> qtl_enrichment_workhorse(
     double a1_est_ns = a1_est;
     double sd1_ns = sd1;
 
-    // apply shrinkage
-    if (prior_variance > 0)
-    {
+    // Apply shrinkage
+    if (prior_variance > 0) {
         double post_var = 1.0 / (1.0 / prior_variance + 1 / (sd1 * sd1));
         a1_est = (a1_est_ns * prior_variance) / (prior_variance + sd1_ns * sd1_ns);
         sd1 = sqrt(post_var);
@@ -174,13 +169,12 @@ std::map<std::string, double> qtl_enrichment_workhorse(
 
     a0_est = log(pi_gwas / (1 + pi_qtl * exp(a1_est) - pi_qtl - pi_gwas));
 
-	double p1 = (1-pi_qtl)* exp(a0_est)/(1+exp(a0_est));
-	double p2 = pi_qtl/(1+exp(a0_est+a1_est));
-	double p12 = pi_qtl*exp(a0_est+a1_est)/(1+exp(a0_est+a1_est));	
+    double p1 = (1 - pi_qtl) * exp(a0_est) / (1 + exp(a0_est));
+    double p2 = pi_qtl / (1 + exp(a0_est + a1_est));
+    double p12 = pi_qtl * exp(a0_est + a1_est) / (1 + exp(a0_est + a1_est));
 
-	double pi1_e = exp(a0_est+a1_est)/(1+exp(a0_est + a1_est));
-	double pi1_ne =  exp(a0_est)/(1+exp(a0_est));
-
+    double pi1_e = exp(a0_est + a1_est) / (1 + exp(a0_est + a1_est));
+    double pi1_ne = exp(a0_est) / (1 + exp(a0_est));
 
     // Create the map to store output
     std::map<std::string, double> output_map;
@@ -196,41 +190,35 @@ std::map<std::string, double> qtl_enrichment_workhorse(
     output_map["pi1_ne"] = pi1_ne;
 
     return output_map;
-
 }
 
-
 // [[Rcpp::export]]
-void qtl_enrichment(SEXP r_gwas_pip, SEXP r_susie_fit_list, 
-                    double pi_gwas = 0, double pi_eqtl = 0, 
-                    int ImpN = 10, double prior_variance = 1) {
-  
+Rcpp::List qtl_enrichment(
+    SEXP r_gwas_pip, SEXP r_susie_fit_list,
+    double pi_gwas = 0, double pi_eqtl = 0,
+    int ImpN = 10, double prior_variance = 1, 
+    int num_threads = 4)
+{
     // Convert r_gwas_pip to C++ type
     std::vector<double> gwas_pip = Rcpp::as<std::vector<double>>(r_gwas_pip);
     std::vector<std::string> gwas_pip_names = Rcpp::as<std::vector<std::string>>(Rcpp::rownames(r_gwas_pip));
-  
+
     // Convert r_susie_fit_list to C++ type
     Rcpp::List susie_fit_list(r_susie_fit_list);
     std::vector<SuSiEFit> susie_fits;
-  
+
     for (int i = 0; i < susie_fit_list.size(); ++i) {
         SuSiEFit susie_fit(susie_fit_list[i]);
         susie_fits.push_back(susie_fit);
     }
 
-/*
-  // For demonstration, print out the first element's pip values and names
-  Rcpp::Rcout << "First susie_fit pip values: ";
-  for (double value : susie_fits[0].pip) {
-    Rcpp::Rcout << value << " ";
-  }
-  Rcpp::Rcout << "\nFirst susie_fit pip names: ";
-  for (std::string name : susie_fits[0].pip_names) {
-    Rcpp::Rcout << name << " ";
-  }
-  Rcpp::Rcout << std::endl;
-*/
+    std::map<std::string, double> output = qtl_enrichment_workhorse(susie_fits, gwas_pip, gwas_pip_names, pi_gwas, pi_eqtl, ImpN, prior_variance, num_threads);
 
-    // Call the workhorse function
-std::map<std::string, double> output = qtl_enrichment_workhorse(susie_fits, gwas_pip, gwas_pip_names, pi_gwas, pi_eqtl, ImpN, prior_variance);
+    // Convert std::map to Rcpp::List
+    Rcpp::List output_list;
+    for (auto const& element : output) {
+        output_list[element.first] = element.second;
+    }
+
+    return output_list;
 }
