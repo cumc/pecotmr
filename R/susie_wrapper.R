49d595fe89e1237047bedbc326016d431145f8a3
hs3393 <hs3393@cumc.columbia.edu>
2024-01-25 20:53:09 -0500

    wrapper modification and bug fix

--- R/susie_wrapper.R
+++ R/susie_wrapper.R
@@ -77,7 +77,7 @@ susie_wrapper = function(X, y, init_L = 10, max_L = 30, coverage = 0.95, max_ite
 #'
 #' @param susie_output Output from running susieR::susie() or susieR::susie_rss()
 #' @param data_x Genotype data matrix for 'susie' or Xcorr matrix for 'susie_rss'.
-#' @param data_y Phenotype data vector for 'susie' or summary stats object for 'susie_rss'.
+#' @param data_y Phenotype data vector for 'susie' or summary stats object for 'susie_rss' (a list contain attribute betahat and sebetahat AND/OR z). i.e. data_y = list(betahat = ..., sebetahat = ...)
 #' @param X_scalar Scalar for the genotype data, used in residual scaling.
 #' @param y_scalar Scalar for the phenotype data, used in residual scaling.
 #' @param maf Minor Allele Frequencies vector.
@@ -176,11 +176,17 @@ susie_post_processor <- function(susie_output, data_x, data_y, X_scalar, y_scala
         pip <- susie_output$pip[top_loci$variant_idx]
         y_scalar <- if (is.null(y_scalar) || all(y_scalar == 1)) 1 else y_scalar[top_loci$variant_idx]
         X_scalar <- if (is.null(X_scalar) || all(X_scalar == 1)) 1 else X_scalar[top_loci$variant_idx]
-        top_loci_cols <- c("variant_id", if (!is.null(maf)) "maf", "bhat", "sbhat", "pip", colnames(top_loci)[-1])
-        res$top_loci <- data.frame(variants,
-                                    res$sumstats$betahat[top_loci$variant_idx] * y_scalar / X_scalar, 
-                                    res$sumstats$sebetahat[top_loci$variant_idx] * y_scalar / X_scalar, 
-                                    pip, stringsAsFactors = FALSE)
+        top_loci_cols <- c("variant_id", "pip" , if (!is.null(res$sumstats$betahat)) "betahat", if (!is.null(res$sumstats$sebetahat)) "sebetahat", if (!is.null(res$sumstats$z)) "z", if (!is.null(maf)) "maf" , colnames(top_loci)[-1])
+        res$top_loci <- data.frame(variants,pip, stringsAsFactors = FALSE)
+        if(mode == "susie"){
+        res$top_loci$betahat = res$sumstats$betahat[top_loci$variant_idx] * y_scalar / X_scalar
+        res$top_loci$sebetahat = res$sumstats$sebetahat[top_loci$variant_idx] * y_scalar / X_scalar
+        }else{
+        res$top_loci$betahat = if (!is.null(res$sumstats$betahat)) res$sumstats$betahat[top_loci$variant_idx] else NULL
+        res$top_loci$sebetahat = if (!is.null(res$sumstats$sebetahat)) res$sumstats$sebetahat[top_loci$variant_idx] else NULL
+        res$top_loci$z = if (!is.null(res$sumstats$z)) res$sumstats$z[top_loci$variant_idx] else NULL
+        }
+                                     
         res$top_loci$maf = if (!is.null(maf)) maf[top_loci$variant_idx] else NULL
         res$top_loci = cbind(res$top_loci , top_loci[,-1])
         colnames(res$top_loci) <- top_loci_cols
