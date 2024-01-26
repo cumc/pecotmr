d735514692c39b2d42d9d0889729110993270181
hs3393 <hs3393@cumc.columbia.edu>
2024-01-25 21:06:41 -0500

    change column

--- R/susie_wrapper.R
+++ R/susie_wrapper.R
@@ -176,12 +176,13 @@ susie_post_processor <- function(susie_output, data_x, data_y, X_scalar, y_scala
         pip <- susie_output$pip[top_loci$variant_idx]
         y_scalar <- if (is.null(y_scalar) || all(y_scalar == 1)) 1 else y_scalar[top_loci$variant_idx]
         X_scalar <- if (is.null(X_scalar) || all(X_scalar == 1)) 1 else X_scalar[top_loci$variant_idx]
-        top_loci_cols <- c("variant_id", "pip" , if (!is.null(res$sumstats$betahat)) "betahat", if (!is.null(res$sumstats$sebetahat)) "sebetahat", if (!is.null(res$sumstats$z)) "z", if (!is.null(maf)) "maf" , colnames(top_loci)[-1])
-        res$top_loci <- data.frame(variants,pip, stringsAsFactors = FALSE)
+        top_loci_cols <- c("variant_id" , if (!is.null(res$sumstats$betahat)) "betahat", if (!is.null(res$sumstats$sebetahat)) "sebetahat", if (!is.null(res$sumstats$z)) "z", if (!is.null(maf)) "maf", "pip" , colnames(top_loci)[-1])
+        res$top_loci <- data.frame(variants, stringsAsFactors = FALSE)
         res$top_loci$betahat = if (!is.null(res$sumstats$betahat)) res$sumstats$betahat[top_loci$variant_idx] * y_scalar / X_scalar else NULL
         res$top_loci$sebetahat = if (!is.null(res$sumstats$sebetahat)) res$sumstats$sebetahat[top_loci$variant_idx]* y_scalar / X_scalar else NULL
         res$top_loci$z = if (!is.null(res$sumstats$z)) res$sumstats$z[top_loci$variant_idx] else NULL
         res$top_loci$maf = if (!is.null(maf)) maf[top_loci$variant_idx] else NULL
+        res$top_loci$pip = pip
         res$top_loci = cbind(res$top_loci , top_loci[,-1])
         colnames(res$top_loci) <- top_loci_cols
         rownames(res$top_loci) <- NULL
