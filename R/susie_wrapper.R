24b8cfa1f79f5ba6d406527647ccfb6cf6e647f6
Gao Wang <wangow@gmail.com>
2024-03-30 17:24:49 -0400

    revert 462630e

--- R/susie_wrapper.R
+++ R/susie_wrapper.R
@@ -200,7 +200,7 @@ susie_rss_wrapper <- function(z, R, n = NULL, var_y = NULL, L = 10, max_L = 30, 
 #'   - `max_L`: Maximum number of causal configurations to consider in the analysis.
 #'   - `l_step`: Step size for increasing L when the limit is reached during dynamic adjustment.
 #'   - `analysis_method`: The analysis method to use. Options are "susie_rss", "single_effect", or "bayesian_conditional_regression".
-#'
+
 #'   The function first checks if the `sumstats` input contains 'z' or 'beta' and 'se' columns. If 'z' is present, it is used directly.
 #'   If 'beta' and 'se' are present, 'z' is calculated as 'beta' divided by 'se'.
 #'
@@ -403,23 +403,16 @@ susie_post_processor <- function(susie_output, data_x, data_y, X_scalar, y_scala
       top_loci[is.na(top_loci)] <- 0
       variants <- res$variant_names[top_loci$variant_idx]
       pip <- susie_output$pip[top_loci$variant_idx]
-      res$top_loci$variant_id <- variants
+      top_loci_cols <- c("variant_id", if (!is.null(res$sumstats$betahat)) "betahat", if (!is.null(res$sumstats$sebetahat)) "sebetahat", if (!is.null(res$sumstats$z)) "z", if (!is.null(maf)) "maf", "pip", colnames(top_loci)[-1])
+      res$top_loci <- data.frame(variants, stringsAsFactors = FALSE)
       res$top_loci$betahat <- if (!is.null(res$sumstats$betahat)) res$sumstats$betahat[top_loci$variant_idx] else NULL
       res$top_loci$sebetahat <- if (!is.null(res$sumstats$sebetahat)) res$sumstats$sebetahat[top_loci$variant_idx] else NULL
       res$top_loci$z <- if (!is.null(res$sumstats$z)) res$sumstats$z[top_loci$variant_idx] else NULL
+      res$top_loci$maf <- if (!is.null(maf)) maf[top_loci$variant_idx] else NULL
       res$top_loci$pip <- pip
-      res$top_loci <- c(res$top_loci, as.list(top_loci[, -1]))
-      res$top_loci$maf <- lapply(maf, function(x) {
-        # Initialize a vector to hold the results for this list element
-        maf_output <- numeric(length(top_loci$variant_idx))
-        # Fill the result vector with NA or the appropriate value from x
-        for (i in seq_along(top_loci$variant_idx)) {
-          idx <- top_loci$variant_idx[i]
-          maf_output[i] <- if (!is.null(idx) && idx > 0 && idx <= length(x)) x[idx] else NA
-        }
-        # Return the result vector for this list element
-        maf_output
-      })
+      res$top_loci <- cbind(res$top_loci, top_loci[, -1])
+      colnames(res$top_loci) <- top_loci_cols
+      rownames(res$top_loci) <- NULL
     }
     names(susie_output$pip) <- NULL
     res$susie_result_trimmed <- list(
