5556214419236ee38c6c172ccde1ae916636544e
Chunmingl <chunming.liu324@gmail.com>
2024-09-24 23:14:33 -0400

    ctwas input formatting update

--- R/ctwas_wrapper.R
+++ R/ctwas_wrapper.R
@@ -6,13 +6,14 @@ ctwas_ld_loader <- function(ld_matrix_file_path) {
   return(ld_loaded)
 }
 #' @importFrom data.table fread
-ctwas_bimfile_loader <- function(ld_matrix_file_path) {
-  snp_info <- fread(paste0(ld_matrix_file_path, ".bim"))
+ctwas_bimfile_loader <- function(bim_file_path) {
+  snp_info <- fread(bim_file_path)
   if (ncol(snp_info) == 9) {
     colnames(snp_info) <- c("chrom", "id", "GD", "pos", "alt", "ref", "variance", "allele_freq", "n_nomiss")
   } else {
     colnames(snp_info) <- c("chrom", "id", "GD", "pos", "alt", "ref")
   }
+  snp_info$id <- gsub("_", ":", gsub("chr", "", snp_info$id))
   return(snp_info)
 }
 
@@ -22,8 +23,9 @@ get_ctwas_meta_data <- function(ld_meta_data_file, regions_table) {
   LD_info <- fread(ld_meta_data_file, header = TRUE, data.table = FALSE)
   colnames(LD_info)[1] <- "chrom"
   LD_info$region_id <- gsub("chr", "", paste(LD_info$chrom, LD_info$start, LD_info$end, sep = "_"))
-  LD_info$LD_matrix <- paste0(dirname(ld_meta_data_file), "/", gsub(",.*$", "", LD_info$path))
-  LD_info <- LD_info[, c("region_id", "LD_matrix")]
+  LD_info$LD_file <- paste0(dirname(ld_meta_data_file), "/", gsub(",.*$", "", LD_info$path))
+  LD_info$SNP_file <- paste0(LD_info$LD_file, ".bim")
+  LD_info <- LD_info[, c("region_id", "LD_file", "SNP_file")]
   region_info <- read.table(regions_table, sep = "\t", header = TRUE) # to get exact LD bim file without over-including neighboring LD's info.
   colnames(region_info)[1] <- "chrom"
   region_info$chrom <- as.integer(gsub("chr", "", region_info$chrom))
