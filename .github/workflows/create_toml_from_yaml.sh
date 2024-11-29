ca7f1092b482f9b3e0f57322b0142c4b3b90a8bb
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-29 13:04:53 -0800

    use rattler-build and pixi for workflows

---
+++ .github/workflows/create_toml_from_yaml.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+local github_workspace=$1
+
+cp ${github_workspace}/.github/environment/pixi.toml ${github_workspace}/pixi.toml
+yq .requirements.host < ${github_workspace}/.github/recipe/recipe.yaml | \
+    sed 's/- //' | grep -v '=' | sed 's/^/"/' | sed 's/$/"/' | sed 's/$/ = "*"/' >> ${github_workspace}/pixi.toml
+yq .requirements.host < ${github_workspace}/.github/recipe/recipe.yaml | \
+    sed 's/- //' | grep '=' | sed 's/^/"/' | sed 's/$/"/' | sed 's/=/" = "/' >> ${github_workspace}/pixi.toml
