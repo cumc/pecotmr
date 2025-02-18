418debc949c18f37714b4fe592932024ccd0a874
Chunmingl <Chunming.liu324@gmail.com>
2025-02-17 23:31:39 -0500

    ctwas extract by study weights

--- R/ctwas_wrapper.R
+++ R/ctwas_wrapper.R
@@ -47,8 +47,8 @@ get_ctwas_meta_data <- function(ld_meta_data_file, subset_region_ids = NULL) {
 trim_ctwas_variants <- function(region_data, twas_weight_cutoff = 1e-5, cs_min_cor = 0.8,
                                 min_pip_cutoff = 0.1, max_num_variants = 1000) {
   # internal functions to select variants for a gene-context pair weight list
-  select_variants <- function(group_name, region_data, cs_min_cor, min_pip_cutoff, max_num_variants) {
-    weight_list <- region_data$weights[[group_name]]
+  select_variants <- function(group_name, study_name, region_data, cs_min_cor, min_pip_cutoff, max_num_variants) {
+    weight_list <- region_data$weights[[group_name]][[study_name]]
     context <- weight_list$context
     selected_variants_by_context <- c()
     molecular_id <- gsub("\\|.*", "", group_name)
@@ -77,23 +77,38 @@ trim_ctwas_variants <- function(region_data, twas_weight_cutoff = 1e-5, cs_min_c
     weight_list$wgt <- weight_list$wgt[selected_variants_by_context, , drop = FALSE]
     return(weight_list)
   }
+  merge_by_study <- function(weights){
+    weight_list<- list()
+    for (group in names(weights)){
+        for (study in names(weights[[group]])){
+            weight_list[[study]][[group]] <- weights[[group]][[study]]
+        }
+    }
+    return(weight_list)
+  }
 
   weights <- setNames(lapply(names(region_data$weights), function(group) {
-    region_data$weights[[group]]$wgt <- region_data$weights[[group]]$wgt[abs(region_data$weights[[group]]$wgt[, 1]) > twas_weight_cutoff, , drop = FALSE]
-    if (nrow(region_data$weights[[group]]$wgt) < 1) {
-      return(NULL)
+    for (study in names(region_data$weights[[group]])){
+      region_data$weights[[group]][[study]]$wgt <- region_data$weights[[group]][[study]]$wgt[abs(region_data$weights[[group]][[study]]$wgt[, 1]) > twas_weight_cutoff, , drop = FALSE]
+      if (nrow(region_data$weights[[group]][[study]]$wgt) < 1) {
+        region_data$weights[[group]][[study]] <- NULL
+        next
+      }
+      if (all(is.na(region_data$weights[[group]][[study]]$wgt[, 1])) || all(is.nan(region_data$weights[[group]][[study]]$wgt[, 1]))) {
+        region_data$weights[[group]][[study]] <- NULL
+        next
+      }
+      if (nrow(region_data$weights[[group]][[study]]$wgt) < max_num_variants) {
+        region_data$weights[[group]][[study]]$n_wgt <- nrow(region_data$weights[[group]][[study]]$wgt)
+      } else {
+        region_data$weights[[group]][[study]] <- select_variants(group, study, region_data, cs_min_cor = cs_min_cor, min_pip_cutoff = min_pip_cutoff, max_num_variants = max_num_variants)
+        region_data$weights[[group]][[study]]$n_wgt <- nrow(region_data$weights[[group]][[study]]$wgt)
+      }
+      region_data$weights[[group]] <- Filter(Negate(is.null), region_data$weights[[group]])
     }
-    if (all(is.na(region_data$weights[[group]]$wgt[, 1])) || all(is.nan(region_data$weights[[group]]$wgt[, 1]))) {
-      return(NULL)
-    }
-    if (nrow(region_data$weights[[group]]$wgt) < max_num_variants) {
-      region_data$weights[[group]]$n_wgt <- nrow(region_data$weights[[group]]$wgt)
-      return(region_data$weights[[group]])
-    }
-    region_data$weights[[group]] <- select_variants(group, region_data, cs_min_cor = cs_min_cor, min_pip_cutoff = min_pip_cutoff, max_num_variants = max_num_variants)
-    region_data$weights[[group]]$n_wgt <- nrow(region_data$weights[[group]]$wgt)
     return(region_data$weights[[group]])
   }), names(region_data$weights))
   weights <- Filter(Negate(is.null), weights)
+  weights <- merge_by_study(weights)
   return(weights)
 }
